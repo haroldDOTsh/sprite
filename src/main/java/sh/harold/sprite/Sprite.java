cf7bfcee1294b2c01408d4f9d016c03979beb484
Harold 🧀 <55301634+ZECHEESELORD@users.noreply.github.com>
2025-11-10 02:36:41 -0500

    initial commit

---
+++ src/main/java/sh/harold/sprite/Sprite.java
@@ -0,0 +1,17 @@
+package sh.harold.sprite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sprite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
